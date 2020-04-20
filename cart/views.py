cd0dded8107c45fc99ec47f78aecefeed0d6f92c
shyam999 <Shyam.acharjya.1@gmail.com>
2020-04-21 03:54:49 +0530

    fixed issues in shop app

--- cart/views.py
+++ cart/views.py
@@ -24,4 +24,4 @@ def cart_detail(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
-    return render(request, 'cart/detail3.html', {'cart': cart})
+    return render(request, 'cart/detail.html', {'cart': cart})
