11aa00b613acc7e058e978ce53fec16f7b52cf0d
shyam999 <Shyam.acharjya.1@gmail.com>
2020-04-24 06:10:56 +0530

    stable

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Category, Product
 
+#register
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
