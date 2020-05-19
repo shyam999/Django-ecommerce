84833205ee990439b1bf66ac7a25069284660cf2
shyam999 <Shyam.acharjya.1@gmail.com>
2020-05-19 14:12:01 +0530

    added class based list and detailview

--- shop/views.py
+++ shop/views.py
@@ -27,8 +27,37 @@ def product_list(request, category_slug=None):
     return render(request, 'shop/product/list.html', context)
 
 
+# class ProductListView(generic.ListView):
+#     template_name = 'shop/product/list.html'
+#     queryset = Product.objects.filter(available=True)
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         category = None
+#         if category_slug:
+#             category = get_object_or_404(Category, slug=category_slug)
+#         context['category'] = category
+#         context['categories'] = Category.objects.all()
+
+
+
+
+
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
     context = {'product': product, 'cart_product_form': cart_product_form}
     return render(request, 'shop/product/detail.html', context)
+
+
+# class ProductDetialView(generic.DetailView):
+
+#     template_name = 'shop/product/detail.html'
+#     model = Product
+#     form_class = CartAddProductForm
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['products'] = get_object_or_404(Product, 
+#         id=id, slug=slug, available=True)
+#         return context
